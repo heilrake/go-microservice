ec2ca8a0321c074bf2164602042e820d8ba46286
heilrake <spemisp40@gmail.com>
2026-01-02 09:35:51 +0200

    update nextjs version

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,4 +1,5 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
@@ -6,42 +7,34 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    rules: {
-      'simple-import-sort/imports': [
-        'error',
-        {
-          groups: [
-            ['^react', '^next', '^@?\\w'],
-            ['^@/app'],
-            ['^@/core'],
-            ['^@/features'],
-            ['^$'],
-            ['^@/shared'],
-            ['^'],
-          ],
-        },
-      ],
-      'simple-import-sort/exports': 'error',
-      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports' },
-      ],
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  plugins: {
+    'simple-import-sort': simpleImportSort,
   },
-  {
-    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react', '^next', '^@?\\w'],
+          ['^@/app'],
+          ['^@/core'],
+          ['^@/features'],
+          ['^$'],
+          ['^@/shared'],
+          ['^'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' },
+    ],
   },
-];
+}, {
+  ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+}];
 
 export default eslintConfig;
